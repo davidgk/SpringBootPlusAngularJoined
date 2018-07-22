c32ce649851308d1ea934b41e67a09ae7e304a04
David Kotlirevsky <david.kotlirevsky@gmail.com>
2018-07-21 23:19:57 -0300

    First Commit

---
+++ backend/src/main/java/com/example/example_greetings/ExampleGreetingsApplication.java
@@ -0,0 +1,12 @@
+package com.example.example_greetings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleGreetingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleGreetingsApplication.class, args);
+	}
+}

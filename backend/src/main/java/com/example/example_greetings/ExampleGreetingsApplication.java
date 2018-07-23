f9fb52d2f93f8b3508e9c300e6847e8f67ed2070
David Kotlirevsky <david.kotlirevsky@gmail.com>
2018-07-23 18:24:11 -0300

    Configure App to Run on Jboss

--- backend/src/main/java/com/example/example_greetings/ExampleGreetingsApplication.java
+++ backend/src/main/java/com/example/example_greetings/ExampleGreetingsApplication.java
@@ -2,9 +2,19 @@ package com.example.example_greetings;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+/**
+ * extending SpringBootServletInitializer as it needed for deploying in
+ * Jboss 7
+ */
 @SpringBootApplication
-public class ExampleGreetingsApplication {
+public class ExampleGreetingsApplication extends SpringBootServletInitializer {
+
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ExampleGreetingsApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExampleGreetingsApplication.class, args);
